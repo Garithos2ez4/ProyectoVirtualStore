b43cf7fca853dc37733842545a6cee374908fe92
Luigui <luigui2ez4me@gmail.com>
2025-06-15 22:31:53 -0500

    Terminado los NavigationDrawer y BottomNavigationBar de la vista vendedor

---
+++ app/src/main/java/com/example/sistemadefacturacion_pruni/Vendedor/Botton_Nav_Fragments_Vendedor/FragmentOrdenesV.kt
@@ -0,0 +1,27 @@
+package com.example.sistemadefacturacion_pruni.Vendedor.Botton_Nav_Fragments_Vendedor
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.sistemadefacturacion_pruni.R
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FragmentOrdenesV.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FragmentOrdenesV : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ordenes_v, container, false)
+    }
+}
+
