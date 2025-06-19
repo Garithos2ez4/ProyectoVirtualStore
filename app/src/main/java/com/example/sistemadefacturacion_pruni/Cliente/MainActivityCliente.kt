a6f0f5ef7e3898d7285bf6b077ddb1ee36a1fe1b
Luigui <luigui2ez4me@gmail.com>
2025-06-18 23:16:54 -0500

    Terminado el diseño de cliente

--- app/src/main/java/com/example/sistemadefacturacion_pruni/Cliente/MainActivityCliente.kt
+++ app/src/main/java/com/example/sistemadefacturacion_pruni/Cliente/MainActivityCliente.kt
@@ -1,16 +1,17 @@
-package com.example.sistemadefacturacion_pruni
+package com.example.sistemadefacturacion_pruni.Cliente
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.sistemadefacturacion_pruni.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivityCliente : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_cliente)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/sistemadefacturacion_pruni/Vendedor/LoginVendedorActivity.kt
+++ app/src/main/java/com/example/sistemadefacturacion_pruni/Vendedor/LoginVendedorActivity.kt
@@ -1,18 +1,84 @@
 package com.example.sistemadefacturacion_pruni.Vendedor
 
+import android.app.ProgressDialog
+import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.sistemadefacturacion_pruni.R
+import com.example.sistemadefacturacion_pruni.databinding.ActivityLoginVendedorBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginVendedorActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginVendedorBinding
+    private lateinit var firebaseAuth : FirebaseAuth
+    private lateinit var progressDialog: ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login_vendedor)
+        binding = ActivityLoginVendedorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        firebaseAuth = FirebaseAuth.getInstance()
 
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Por favor espere")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        binding.btnLoginV.setOnClickListener {
+          validarInfo()
+        }
+        binding.tvRegistrarV.setOnClickListener {
+            startActivity(Intent(applicationContext,RegistroVendedorActivity::class.java))
+        }
+
+    }
+    private var email = ""
+    private var password = ""
+    private fun validarInfo(){
+        email = binding.etEmailV.text.toString().trim()
+        password = binding.etPassword.text.toString().trim()
+
+        if (email.isEmpty()){
+            binding.etEmailV.error="Ingrese su email"
+            binding.etEmailV.requestFocus()
+        }else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            binding.etEmailV.error="Email invalido"
+            binding.etEmailV.requestFocus()
+        }else if (password.isEmpty()){
+            binding.etPassword.error="Ingrese su contraseña"
+            binding.etPassword.requestFocus()
+        }else{
+            loginVendedor()
+
+        }
+
+    }
+
+    private fun loginVendedor() {
+        progressDialog.setMessage("Ingresando")
+        progressDialog.show()
+
+        firebaseAuth.signInWithEmailAndPassword(email,password)
+            .addOnSuccessListener {
+                progressDialog.dismiss()
+                startActivity(Intent(this,MainActivityVendedor::class.java))
+                finishAffinity()
+                Toast.makeText(
+                    this,
+                    "Bienvenido(a)",
+                    Toast.LENGTH_SHORT
+
+                ).show()
+            }
+            .addOnFailureListener { e->
+                Toast.makeText(this,"Fallo el login debido a ${e.message}",
+                    Toast.LENGTH_SHORT).show()
+            }
     }
 }
