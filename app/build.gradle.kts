262a6353a9a05d3f2dc0d4fa44ee8bad1bc208c9
Luigui <luigui2ez4me@gmail.com>
2025-06-23 21:17:05 -0500

    Agregado la libreria image picker y Abrir galería o cámara del dispositivo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.auth.ktx)
+    implementation(libs.imagePicker)/*Recortar una imagen del celular*/
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/sistemadefacturacion_pruni/Vendedor/Productos/AgregarProductoActivity.kt
+++ app/src/main/java/com/example/sistemadefacturacion_pruni/Vendedor/Productos/AgregarProductoActivity.kt
@@ -1,13 +1,48 @@
 package com.example.sistemadefacturacion_pruni.Vendedor.Productos
 
+import android.app.Activity
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import com.example.sistemadefacturacion_pruni.R
+import com.example.sistemadefacturacion_pruni.databinding.ActivityAgregarProductoBinding
+import com.github.dhaval2404.imagepicker.ImagePicker
 
 class AgregarProductoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAgregarProductoBinding
+    private var imageUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_agregar_producto)
+        binding = ActivityAgregarProductoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imgAgregarProducto.setOnClickListener {
+            seleccionarImg()
+        }
 
     }
+    private fun seleccionarImg(){
+        ImagePicker.with(this)
+            .crop()
+            .compress(1024)
+            .maxResultSize(1080,1080)
+            .createIntent { intent ->
+                resultadoImg.launch(intent)
+            }
+
+    }
+    private val resultadoImg =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if(result.resultCode == Activity.RESULT_OK){
+                val data = result.data
+                imageUri = data!!.data
+                binding.imgAgregarProducto.setImageURI(imageUri)
+            } else {
+                Toast.makeText(this, "Accion cancelada,No selecciono ninguna imagen", Toast.LENGTH_SHORT).show()
+            }
+        }
+
 }
